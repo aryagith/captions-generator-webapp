79fe03abd4bc4d76365427799cf82f652c1d2d7b
arya gosavi <aaryagosavi17@gmail.com>
2024-08-07 06:59:36 -0400

    Working Demo Section

--- src/components/DemoSection.js
+++ src/components/DemoSection.js
@@ -1,15 +1,19 @@
 import SparklesIcon from "./SparklesIcon";
 export default function DemoSection(){
     return(<section className="flex mt-12 justify-around items-center">
-        <div className="bg-gray-800/50 w-[240px] h-[480px] rounded-xl">
+        <div className=" w-[240px] h-[425px] rounded-xl overflow-hidden">
         <video 
-            src="https://github.com/aryagith/captions-generator-webapp/blob/master/src/vids/TikVid.io_7371953577163492613.mp4"
-            autoplay
+            src="https://captioner-video.s3.ca-central-1.amazonaws.com/TikVid.io_7371953577163492613.mp4"
+            autoPlay
+            loop
+            muted
             ></video></div>
         <SparklesIcon />
-        <div className="bg-gray-800/50 w-[240px] h-[480px] rounded-xl"><video 
-            src="https://github.com/aryagith/captions-generator-webapp/blob/master/src/vids/transcribed.mp4"
-            autoplay
+        <div className=" w-[240px] h-[425px] rounded-xl overflow-hidden"><video 
+            src="https://captioner-video.s3.ca-central-1.amazonaws.com/transcribed.mp4"
+            autoPlay
+            loop
+            muted
             ></video></div>
       </section>);
 }
