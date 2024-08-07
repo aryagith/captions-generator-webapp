d80a258efd26db776bc8094c1e8440e7d0f706ac
arya gosavi <aaryagosavi17@gmail.com>
2024-08-07 06:34:34 -0400

    test demo videos

--- src/components/DemoSection.js
+++ src/components/DemoSection.js
@@ -1,8 +1,15 @@
 import SparklesIcon from "./SparklesIcon";
 export default function DemoSection(){
     return(<section className="flex mt-12 justify-around items-center">
-        <div className="bg-gray-800/50 w-[240px] h-[480px] rounded-xl"></div>
+        <div className="bg-gray-800/50 w-[240px] h-[480px] rounded-xl">
+        <video 
+            src="https://github.com/aryagith/captions-generator-webapp/blob/master/src/vids/TikVid.io_7371953577163492613.mp4"
+            autoplay
+            ></video></div>
         <SparklesIcon />
-        <div className="bg-gray-800/50 w-[240px] h-[480px] rounded-xl"></div>
+        <div className="bg-gray-800/50 w-[240px] h-[480px] rounded-xl"><video 
+            src="https://github.com/aryagith/captions-generator-webapp/blob/master/src/vids/transcribed.mp4"
+            autoplay
+            ></video></div>
       </section>);
 }
